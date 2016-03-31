fee0648a96122f5f0076185b3d6a0840b49276a9
sgmt <doku0911@hotmail.co.jp>
2016-03-31 21:18:24 +0900

    players Movement and cameras Movement inserted

---
+++ Assets/Script/Camera/CameraMovementController.cs
@@ -0,0 +1,157 @@
+﻿/*
+ * @file CameraMovementController.cs
+ * @brief カメラの移動操作に関する操作を管理するスクリプト
+ * @date 2016-03-28
+ * @auther kondo
+ */
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// カメラの移動に関するクラス
+/// </summary>
+public class CameraMovementController : MonoBehaviour {
+
+    /// <summary>
+    /// 注視する対象（プレイヤー）
+    /// </summary>
+    [SerializeField]
+    Transform target = null;
+
+    /// <summary>
+    /// 横方向のカメラの回転速度
+    /// </summary>
+    [Tooltip("横方向のカメラの回転速度")]
+    public float rotateAnglePerSecond = 10f;
+    
+    /// <summary>
+    /// カメラが見下げ（見上げ）ている角度
+    /// 1で直上、0で真横となる。
+    /// </summary>
+    [SerializeField, Range(0f, 1f)]
+    float cameraUpperAngle = 0.5f;
+
+    float cameraHorizontalAngle = 0f;
+    
+    /// <summary>
+    /// カメラの対象への距離
+    /// </summary>
+    [SerializeField, Tooltip("カメラの対象への距離")]
+    float cameraDistance = 10f;
+
+    enum STATE
+    {
+        UnderControl,   //操作可能
+        OutOfControl
+    }
+
+    STATE state = STATE.OutOfControl;
+
+    /// <summary>
+    /// 起動時にカメラをコントロール出来る状態にするかどうか
+    /// </summary>
+    [SerializeField]
+    bool UnderControlOnAwake = true;
+
+	// Use this for initialization
+	void Start () {
+
+        if (zoomRateArray.Length == 0)
+        {
+            Debug.LogError("ZoomRateを登録してください！");
+        }
+        else
+        {
+            cameraDistance = moveToZoomRate = zoomRateArray[0];
+
+        }
+
+        if (UnderControlOnAwake)
+        {
+            state = STATE.UnderControl;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        switch (state)
+        {
+            case STATE.UnderControl:
+                Zoom();
+                RotateAroundTarget();
+                transform.LookAt(target);
+
+                break;
+
+            case STATE.OutOfControl:
+                break;
+
+        }
+	}
+
+
+    /// <summary>
+    /// targetを中心に回転運動
+    /// </summary>
+    void RotateAroundTarget()
+    {
+
+        cameraHorizontalAngle += Input.GetAxisRaw("CameraRotateHorizontal") * Time.deltaTime;
+        cameraHorizontalAngle %= 1f;
+
+        float verticalDistance = Mathf.Sin(cameraUpperAngle * Mathf.PI * 0.5f) * cameraDistance;
+
+        float horizontalDistance = Mathf.Cos(cameraUpperAngle * Mathf.PI * 0.5f) * cameraDistance;
+        float XDistance = Mathf.Cos(cameraHorizontalAngle * Mathf.PI * 2f) * horizontalDistance;
+        float ZDistance = Mathf.Sin(cameraHorizontalAngle * Mathf.PI * 2f) * horizontalDistance;
+
+
+        transform.position = target.position + new Vector3(XDistance, verticalDistance, ZDistance);
+
+
+    }
+
+    /// <summary>
+    /// 拡大率（カメラの距離)の一覧
+    /// </summary>
+    [SerializeField]
+    float[] zoomRateArray;
+
+    /// <summary>
+    /// するべき拡大率
+    /// </summary>
+    float moveToZoomRate;
+
+    /// <summary>
+    /// 現在設定されている拡大率の配列の番号
+    /// </summary>
+    int zoomRateIndex = 0;
+
+    [SerializeField]
+    float zoomLerpRate = 0.95f;
+
+    void Zoom()
+    {
+
+
+
+        if (InputManager.IsDown(INPUT_ID.ZOOM))
+        {
+            zoomRateIndex++;
+
+            if (zoomRateArray.Length == zoomRateIndex)
+            {
+                zoomRateIndex = 0;
+            }
+            moveToZoomRate = zoomRateArray[zoomRateIndex];
+        }
+
+
+        cameraDistance = Mathf.Lerp(cameraDistance, moveToZoomRate, zoomLerpRate);
+
+    }
+
+}
---
+++ Assets/Script/Player/PlayerMovementController.cs
@@ -0,0 +1,162 @@
+﻿/* 
+ * @file PlayerMovementController.cs
+ * @brief プレイヤーの移動に関する操作を管理するスクリプト
+ * @date 2016-03-28
+ * @auther kondo
+ */
+
+
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// プレイヤーの移動に関するクラス
+/// </summary>
+public class PlayerMovementController : MonoBehaviour {
+
+
+    /// <summary>
+    /// プレイヤーの移動方向
+    /// </summary>
+    Vector3 moveVelocity = Vector3.zero;
+
+    /// <summary>
+    /// プレイヤーの移動速度
+    /// </summary>
+    float moveSpeed = 10f;
+
+    /// <summary>
+    /// プレイヤーの状態一覧
+    /// </summary>
+    enum STATE
+    {
+        Unactive = -1,
+        Active = 1,
+    }
+
+    /// <summary>
+    /// プレイヤーの状態を管理する
+    /// </summary>
+    STATE state = STATE.Unactive;
+
+    [SerializeField,Tooltip("開始時にプレイヤーを操作可能かどうか")]
+    bool ActiveOnAwake = true;
+
+    /// <summary>
+    /// 自分のRigidBody
+    /// </summary>
+    Rigidbody myRigid = null;
+
+    /// <summary>
+    /// カメラの情報を得る
+    /// </summary>
+    Transform cameraTransform = null;
+
+    /// <summary>
+    /// ジャンプする力
+    /// </summary>
+    [SerializeField]
+    float jumpPower = 5f;
+
+    [SerializeField]
+    float gravityPower = 9.8f;
+
+    /// <summary>
+    /// 地面に触れているかどうか≒ジャンプが出来る状態
+    /// </summary>
+    bool isTouchingFloor = true;
+
+
+    void Start () {
+
+        Physics.gravity = new Vector3(0, gravityPower, 0);
+
+
+        AttachComponents();
+        LoadParameters();
+
+        if (ActiveOnAwake)
+        {
+            state = STATE.Active;
+        }
+	}
+	
+	void FixedUpdate () {
+
+        switch (state)
+        {
+            case STATE.Unactive:
+                break;
+
+            case STATE.Active:
+                RefreshVelocity();
+                Move();
+                Jump();
+
+                break;
+        }
+
+	}
+
+    /// <summary>
+    /// GetComponent()等あてはめ処理を行います。
+    /// </summary>
+    void AttachComponents()
+    {
+        myRigid = GetComponent<Rigidbody>();
+        cameraTransform = Camera.main.transform;
+
+    }
+
+    /// <summary>
+    /// パラメータを読み込みあてはめます
+    /// </summary>
+    void LoadParameters()
+    {
+        moveSpeed = GameCharacterParam.GetMoveSpeed();
+
+    }
+
+    /// <summary>
+    /// 入力等をもとにプレイヤーの移動方向を決定する
+    /// </summary>
+    void RefreshVelocity()
+    {
+        moveVelocity = cameraTransform.right * InputManager.GetAxisRaw(INPUT_ID.PLAYER_MOVE_HORIZONTAL) - cameraTransform.forward * InputManager.GetAxisRaw(INPUT_ID.PLAYER_MOVE_VERTICAL);
+        moveVelocity = new Vector3(moveVelocity.x, 0, moveVelocity.z).normalized;
+    }
+
+    /// <summary>
+    /// 移動させる
+    /// </summary>
+    void Move()
+    {
+        //RigidBodyを使う場合の処理
+        myRigid.AddForce(moveVelocity * moveSpeed, ForceMode.VelocityChange);
+
+    }
+
+    /// <summary>
+    /// ジャンプ
+    /// </summary>
+    void Jump()
+    {
+        if (InputManager.IsDown(INPUT_ID.PLAYER_JUMP) && isTouchingFloor)
+        {
+            myRigid.AddForce(Vector3.up * jumpPower, ForceMode.VelocityChange);
+            isTouchingFloor = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+
+        ///レイヤーやタグでやるなら変更すること
+        if (coll.transform.tag == "Floor")
+        {
+            isTouchingFloor = true;
+        }
+
+    }
+
+}
