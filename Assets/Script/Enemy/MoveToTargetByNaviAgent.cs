df14a146ad54e01642d680bfa1d9edbf070371bb
sgmt <doku0911@hotmail.co.jp>
2016-05-23 00:16:59 +0900

    navMesh attached not good

---
+++ Assets/Script/Enemy/MoveToTargetByNaviAgent.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveToTargetByNaviAgent : MonoBehaviour {
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    bool moveOnAwake = false;
+
+    public bool CanMoveToTarget { get; set; }
+
+    [SerializeField]
+    float minDistance = 2f;
+
+    NavMeshAgent navMeshAgent = null;
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        if(moveOnAwake)
+        {
+            CanMoveToTarget = true;
+            navMeshAgent.destination = target.position;
+        }
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (CanMoveToTarget)
+        {
+            MoveToTarget();
+        }
+
+
+        if (Vector3.Distance(transform.position, target.position) < minDistance)
+        {
+            CanMoveToTarget = false;
+            navMeshAgent.Stop();
+        }
+        else
+        {
+            CanMoveToTarget = true;
+            navMeshAgent.Resume();
+        }
+
+	}
+
+    void MoveToTarget()
+    {
+        navMeshAgent.destination = target.position;
+    }
+}
---
+++ Assets/Script/Light/RandomIntensity.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomIntensity : MonoBehaviour {
+
+
+    [SerializeField]
+    float minIntensity = 0.0f;
+
+    [SerializeField]
+    float maxIntensity = 5.0f;
+
+    [SerializeField]
+    float changeInterval = 0.5f;
+
+    float changedCoolTime = 0f;
+
+
+    Light thisLight = null;
+
+    void Start()
+    {
+        thisLight = GetComponent<Light>();
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        changedCoolTime += Time.deltaTime;
+
+	    if(changedCoolTime >= changeInterval)
+        {
+            thisLight.intensity = Random.Range(minIntensity, maxIntensity);
+            changedCoolTime = 0f;
+        }
+	}
+}
