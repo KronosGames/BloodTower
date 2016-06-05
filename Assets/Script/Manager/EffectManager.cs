0bfffc4a713c6d52e7dc3226930a5b9e8910c4a3
syougun360 <syougun360@infoseek.jp>
2016-06-05 12:17:45 +0900

    エフェクト実装

--- Assets/Script/Manager/EffectManager.cs
+++ Assets/Script/Manager/EffectManager.cs
@@ -4,8 +4,15 @@ using System.Collections.Generic;
 
 public enum EFFECT_ID
 {
-    NULL,   //< 未定義
-    FLAME,  //< 炎
+    NULL,           //< 未定義
+    FLAME,          //< 炎
+    BIG_SPLASH,     //< 大きい血しぶき
+    SMALL_SPLASH,   //< 小さい血しぶき
+    BIG_SMOKE,      //< 大きい煙
+    SMALL_SMOKE,    //< 小さい煙
+    WEAPON_HIT,     //< 武器オブジェクトヒット
+    WATER_SPILL,    //< 水こぼれたとき
+    WATER_SUPPLY,   //< 聖水補給
 }
 
 public class ParticleSystemData
@@ -18,6 +25,7 @@ public class ParticleSystemData
 public class EffectDatabase
 {
     public EFFECT_ID effectID = EFFECT_ID.NULL;
+    public int createCount = 3;
     public GameObject prefab = null;
 }
 
@@ -26,26 +34,25 @@ public class EffectData
     public EFFECT_ID effectID = EFFECT_ID.NULL;
     public GameObject effectObject = null;
     public ParticleSystemData particleSystemData = new ParticleSystemData();
-    public EffectMover effectMove = null;
 
+    public int playIndex = -1;
     public bool isActive = false;
     public bool isAutoDestroy = false;
 }
 
 public class EffectManager : ManagerBase
 {
-    static readonly Vector3 LostPosition = new Vector3(5000, 0, 0);
-
-    [SerializeField]
-    int createNum = 5;
+    static readonly Vector3 LostPosition = new Vector3(50000, 0, 0);
 
     [SerializeField]
     List<EffectDatabase> setEffectDBList = new List<EffectDatabase>();
 
     static List<EffectData> effectDataList = new List<EffectData>();
+    static int playIndexCount = -1;
 
-	void Start ()
+    void Start ()
     {
+        playIndexCount = -1;
         effectDataList.Clear();
 
         InitManager(this, MANAGER_ID.EFFECT);
@@ -53,7 +60,7 @@ public class EffectManager : ManagerBase
         for (int createIndex = 0; createIndex < setEffectDBList.Count; createIndex++)
         {
             var db = setEffectDBList[createIndex];
-            for (int i = 0; i < createNum; i++)
+            for (int i = 0; i < db.createCount; i++)
             {
                 EffectData data = new EffectData();
 
@@ -61,14 +68,11 @@ public class EffectManager : ManagerBase
                 GameObject obj = Instantiate(db.prefab) as GameObject;
                 obj.transform.SetParent(transform);
                 obj.name = db.prefab.name + i.ToString("00");
-                obj.transform.localPosition = Vector3.zero;
-                obj.transform.localScale = Vector3.zero;
-                obj.transform.localRotation = Quaternion.identity;
+                obj.transform.localPosition = LostPosition;
                 data.effectObject = obj;
-                data.effectMove = obj.GetComponent<EffectMover>();
 
                 ParticleSystemData particleData = data.particleSystemData;
-                particleData.trans = obj.transform.GetChild(0);
+                particleData.trans = obj.transform;
                 particleData.particleSystem = particleData.trans.GetComponent<ParticleSystem>();
                 particleData.particleSystem.Stop();
 
@@ -84,48 +88,57 @@ public class EffectManager : ManagerBase
     {
         for (int i = 0; i < effectDataList.Count; i++)
         {
-            var data = effectDataList[i];
+            EffectData data = effectDataList[i];
+            if (!data.isActive) continue;
             if (!data.isAutoDestroy) continue;
 
-            if (data.effectMove.IsHit())
+            if (!data.particleSystemData.particleSystem.isPlaying)
             {
-                Stop(ref data);
+                Stop(data.playIndex);
                 return;
             }
         }
     }
 
-    static public EffectData PlayEffect(EFFECT_ID id, Vector3 createPosition, Vector3 targetPosition, float power, bool isAutoDestroy = false)
+    static public int PlayEffect(EFFECT_ID id, Vector3 createPosition, bool isAutoDestroy = false)
     {
         for (int i = 0; i < effectDataList.Count; i++)
         {
-            var data = effectDataList[i];
+            EffectData data = effectDataList[i];
             
             if(data.effectID != id) continue;
             if(data.isActive) continue;
 
+            playIndexCount++;
+
             data.isActive = true;
+            data.effectObject.SetActive(true);
 
             data.effectObject.transform.position = createPosition;
-            data.effectMove.SetTargetPosition(targetPosition, power);
             data.particleSystemData.particleSystem.Play();
 
-            data.effectObject.SetActive(true);
             data.isAutoDestroy = isAutoDestroy;
+            data.playIndex = playIndexCount;
 
-            return data;
+            return data.playIndex;
         }
 
-        return null;
-
+        return -1;
     }
 
-    static public void Stop(ref EffectData playData)
+    static public void Stop(int playIndex)
     {
-        playData.isActive = false;
-        playData.particleSystemData.particleSystem.Stop();
-        playData.effectObject.SetActive(false);
-        playData.effectObject.transform.position = LostPosition;
+        for (int i = 0; i < effectDataList.Count; i++)
+        {
+            EffectData data = effectDataList[i];
+            if (data.playIndex != playIndex) continue;
+
+            data.playIndex = -1;
+            data.isActive = false;
+            data.particleSystemData.particleSystem.Stop();
+            data.effectObject.SetActive(false);
+            data.effectObject.transform.position = LostPosition;
+        }
     }
 
 }
